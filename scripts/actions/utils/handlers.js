48e6043bed05111694d18fe7e633df4398d0f3e1
Tabatha Zeitke <tzeitke@newrelic.com>
2023-06-09 11:48:32 -0700

    feat: Create comment component for markdown pages
     - this component does not render in the UI
     - this component is not translated

--- scripts/actions/utils/handlers.js
+++ scripts/actions/utils/handlers.js
@@ -120,6 +120,14 @@ module.exports = {
         wrapChildren: false,
       }),
   },
+  WARNING: {
+    deserialize: deserializeComponent,
+    serialize: (h, node) =>
+      serializeComponent(h, node, {
+        classNames: 'notranslate',
+        wrapChildren: false,
+      }),
+  },
   ExternalLink: {
     deserialize: deserializeComponent,
     serialize: serializeComponent,
